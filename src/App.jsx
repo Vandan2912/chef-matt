1a54d53f5bd748c3d6cdd5fbcea40817e05c6862
jagtapyogesh41 <dfhunnypatel@gmail.com>
2024-07-20 13:34:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div id="main-app">
+      <Sidebar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
